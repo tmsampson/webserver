24fb6dd1cb3da481275fbac028c94a5e4e0a51cb
Thomas Sampson <32266729+tmsampson@users.noreply.github.com>
2018-07-19 08:59:12 +0000

    Stripped redundant code

--- webserver.js
+++ webserver.js
@@ -1,9 +1,7 @@
 // Dependencies
-const path = require("path");
-const url = require("url");
 var express = require('express');
 
-// Host image cache on port 3000
+// Host http server on port 3000
 imageServer = express();
 imageServer.use("/", express.static(__dirname));
 imageServer.listen(3000);
