056b80f56228ee4a7c69cd91f046e4cc985a71f7
Thomas Sampson <tmsampson@gmail.com>
2020-07-20 17:49:59 +0100

    Added support for passing root directory on command line

--- webserver.js
+++ webserver.js
@@ -2,6 +2,17 @@
 var express = require('express');
 
 // Host http server on port 3000
-imageServer = express();
-imageServer.use("/", express.static(__dirname));
-imageServer.listen(3000);
+var args = process.argv.slice(2);
+if(args.length == 0)
+{
+	console.log("Please pass webserver root on command line");
+	process.exit(1);
+}
+
+var root = args[0];
+webserver = express();
+webserver.use("/", express.static(root));
+webserver.listen(3000);
+
+// Log
+console.log("Serving " + root + " @ http://localhost:3000");
